4cc4c0c700d589534eddaf04414cf7bbad1a145a
jf <jiangfeng3@163.com>
2019-01-12 20:40:57 +0800

    add markets

--- app/src/main/java/com/fun/finance/myapplication/FinanceActivity.java
+++ app/src/main/java/com/fun/finance/myapplication/FinanceActivity.java
@@ -1,6 +1,8 @@
 package com.fun.finance.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,13 +15,23 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import static android.view.KeyEvent.KEYCODE_BACK;
 
 public class FinanceActivity extends AppCompatActivity {
 
@@ -38,6 +50,8 @@ public class FinanceActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    private Map<String, ?> mConcernedMarket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +59,16 @@ public class FinanceActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        SharedPreferences sp = getSharedPreferences("markets", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sp.edit();
+        editor.putString("黄金","0");
+        editor.putString("原油","0");
+        editor.putString("大豆","0");
+        editor.commit();
+        mConcernedMarket = sp.getAll();
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -67,19 +91,18 @@ public class FinanceActivity extends AppCompatActivity {
                 startActivity(new Intent("android.intent.action.singtop"));
             }
         });
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("JIANG","fun onDestroy");
+        Log.d("JIANG", "fun onDestroy");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("JIANG","fun onStop");
+        Log.d("JIANG", "fun onStop");
     }
 
     @Override
@@ -100,7 +123,6 @@ public class FinanceActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -108,11 +130,14 @@ public class FinanceActivity extends AppCompatActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        WebView mWebView;
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final String ARG_MARKETS = "markets";
+        ArrayList<String> mMarketsList;
 
         public PlaceholderFragment() {
         }
@@ -121,10 +146,20 @@ public class FinanceActivity extends AppCompatActivity {
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int sectionNumber, Map<String, ?> markets) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            ArrayList<String> marketsList = new ArrayList<>();
+            Iterator iter = markets.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                String key = (String) entry.getKey();
+                String val = (String) entry.getValue();
+                marketsList.add(key);
+            }
+
+            args.putStringArrayList(ARG_MARKETS, marketsList);
             fragment.setArguments(args);
             return fragment;
         }
@@ -133,9 +168,31 @@ public class FinanceActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_finance, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            mWebView = (WebView) rootView.findViewById(R.id.webview);
+            mMarketsList = getArguments().getStringArrayList(ARG_MARKETS);
+            int position = getArguments().getInt(ARG_SECTION_NUMBER);
+            if (mMarketsList != null && mMarketsList.size() > 0) {
+                StringBuilder sb = new StringBuilder("https://www.baidu.com/s?wd=");
+                sb.append(mMarketsList.get(position));
+                mWebView.loadUrl(sb.toString());
+                mWebView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                        if (request.isRedirect()) {
+                            view.loadUrl(request.getUrl().toString());
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+            }
+
             return rootView;
+        }
+
+
+        public WebView getWebView() {
+            return mWebView;
         }
     }
 
@@ -144,6 +201,8 @@ public class FinanceActivity extends AppCompatActivity {
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        PlaceholderFragment currentFragment;
+
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -153,13 +212,30 @@ public class FinanceActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return PlaceholderFragment.newInstance(position, mConcernedMarket);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return mConcernedMarket == null ? 0 : mConcernedMarket.size();
         }
+
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            Log.d("JIANG", "setPrimaryItem position:" + position + " object:" + object);
+            currentFragment = (PlaceholderFragment) object;
+            super.setPrimaryItem(container, position, object);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView currentWebView = mSectionsPagerAdapter.currentFragment.getWebView();
+        if ((keyCode == KEYCODE_BACK) && currentWebView.canGoBack()) {
+            currentWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
